8a431610f3e4b8d84486ee2867122449ed371ac0
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2019-11-29 23:21:15 +0900

    Add an analyzer for time width

--- wfsimn/analyzer.py
+++ wfsimn/analyzer.py
@@ -1,7 +1,7 @@
 import logging
 import numpy as np
 from tqdm import tqdm
-
+import wfsimn
 
 class analyzer():
 
@@ -33,4 +33,38 @@ class analyzer():
             hit_numbers.append(len(hit_pmts))
 
         return hit_numbers
+
+    def loop_event_delta_time(self, threshold=0.1):
+
+        delta_times = []
+
+        for wf in tqdm(self.wfs, leave=False):
+
+            pulse = np.sum(wf, axis=1)
+            peak_ADC = pulse.max()
+            peak_timebin = pulse.argmax()
+            under_threshold_indexs = np.where(pulse < peak_ADC * threshold)[0]
+            try:
+                #delta_timebin_lbk = peak_timebin - under_threshold_indexs[under_threshold_indexs < peak_timebin].max()
+                delta_timebin_lfw = under_threshold_indexs[under_threshold_indexs > peak_timebin].min() - peak_timebin
+            except ValueError:
+                continue
+
+            delta_times.append(delta_timebin_lfw * 2)
+
+        return delta_times
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level='DEBUG')
+
+    man = wfsimn.manager()
+    mc_name = 'mc51'
+    batchid = 1
+    man.mc_file_name = '/Users/mzks/xenon/mc/data/' + mc_name + '/output' + str(batchid).zfill(4) + '_Sort.root'
+    man.load_pickle('/Users/mzks/xenon/wfsimn/notebooks/wf_files/' + mc_name + '_' + str(batchid).zfill(4) + '.pkl')
+
+    ana = man.analyzer()
+    delta_times = ana.loop_event_delta_time()
 
